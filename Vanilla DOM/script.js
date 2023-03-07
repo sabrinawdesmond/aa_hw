4b42183caa9b4904f0f5ee4247656779a6632387
Sabrina Desmond <sabrinawdesmond@gmail.com>
2023-03-07 07:39:29 -0500

    vanilla hw

---
+++ Vanilla DOM/script.js
@@ -0,0 +1,32 @@
+//didn't have time to get to this - prioritized sleep
+document.addEventListener("DOMContentLoaded", () => {
+  // toggling restaurants
+
+  const toggleLi = (e) => {
+    const li = e.target;
+    if (li.className === "visited") {
+      li.className = "";
+    } else {
+      li.className = "visited";
+    }
+  };
+
+  document.querySelectorAll("#restaurants li").forEach((li) => {
+    li.addEventListener("click", toggleLi);
+  });
+
+
+
+  // adding SF places as list items
+
+  // --- your code here!
+
+
+
+  // adding new photos
+
+  // --- your code here!
+
+
+
+});
